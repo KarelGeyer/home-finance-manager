5a879df9ee6c62a15191a90c645cc16e57a2a22f
Karel Geyer <karelgeyer@gmail.com>
2022-08-28 20:11:26 +0200

    feat: wrap the app in correct providers to use Context and Redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { store } from "../state/store";
+import { Provider } from "react-redux";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
 
 import MaxWidth from "../components/MaxWidth";
 
 import "../styles/globals.css";
+import { UserSearchContext } from "../state/context/userContext";
 
 const client = new ApolloClient({
   uri: "http://localhost:2000/graphql",
@@ -12,11 +15,17 @@ const client = new ApolloClient({
 });
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const [userSearch, setUserSearch] = useState("");
+
   return (
     <ApolloProvider client={client}>
-      <MaxWidth>
-        <Component {...pageProps} />
-      </MaxWidth>
+      <Provider store={store}>
+        <UserSearchContext.Provider value={{ userSearch, setUserSearch }}>
+          <MaxWidth>
+            <Component {...pageProps} />
+          </MaxWidth>
+        </UserSearchContext.Provider>
+      </Provider>
     </ApolloProvider>
   );
 };
