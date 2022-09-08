37249d0bf31707a496227a70a2adf71289176e12
Karel Geyer <karelgeyer@gmail.com>
2022-09-09 01:56:17 +0200

    feat: introduce new features in trasactions page
    transactions page now fully communicates with it's backend, being able
    to fetch, delete, create and update all teh transactions.
    
    Transactions are also sorted based on users choice of preference and
    only transactions related to chosen month are display.
    
    There is quite some data formatting needed in order to achieve the
    wanted state which probably will be handled on backend in the future as
    it probably will be much  better performace-wise
    
    also modal was removed and one simple form was created directly on the
    page and the graph was seen unneccessary and removed.

--- pages/transactions/index.tsx
+++ pages/transactions/index.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import AppBarMenu from "../../components/AppBar";
-import LineGraph from "../../components/LineGraph";
-import EditTransaction from "../../components/EditTransaction";
 import Transaction, {
   IProps as TransactionProps,
 } from "../../components/Transaction";
@@ -10,20 +8,30 @@ import Transaction, {
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import MenuItem from "@mui/material/MenuItem";
-import Modal from "@mui/material/Modal";
-import CancelIcon from "@mui/icons-material/Cancel";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 import {
   GridContainer,
-  ButtonWithIcon,
-  ButtonWithIconTypography,
   TransactionGrid,
-  FilterSelect,
 } from "../../styles/pages/transactions";
-import { Section, MainHeading, FormBox } from "../../styles/global";
+import {
+  Section,
+  MainHeading,
+  Form,
+  FormInput,
+  FormButton,
+  FormSelect,
+} from "../../styles/global";
 
-import { TRANSACTIONS_URL } from "../../assets/global";
+import { getDate } from "../../helpers";
+import { useMutation, useQuery } from "@apollo/client";
+import {
+  CREATE_TRANSACTION,
+  GET_TRANSACTIONS,
+  GET_USER,
+  UPDATE_TRANSACTION,
+} from "../../graphql";
+import { useSelector } from "react-redux";
+import { Checkbox, FormControlLabel } from "@mui/material";
 
 export interface IProps {
   transactions: Transaction[];
@@ -31,149 +39,324 @@ export interface IProps {
 
 export interface Transaction {
   name: string;
-  person: string;
+  person: {
+    name: string;
+  };
   category: string;
   sum: number;
+  date: string;
   currency: string;
   month?: string;
-  isLoand?: boolean;
+  isLoan?: boolean;
   tags?: string | string[];
   __v?: number;
   _id: string;
 }
 
-const Transactions: React.FC<IProps> = ({ transactions }) => {
-  const [filterValue, setFilterValue] = useState<string>("Group");
-  const [modalOpened, setModalOpened] = useState<boolean>(false);
+export interface TransactionDetails {
+  id: string;
+  name: string;
+  category: string;
+  sum: number;
+  currency: CURRENCY;
+  isLoan?: boolean;
+  date?: string;
+  personId: string;
+}
 
-  const openModal = (): void => setModalOpened(true);
-  const closeModal = (): void => setModalOpened(false);
+export enum CURRENCY {
+  CZK = "CZK",
+  EUR = "EUR",
+}
 
+const Transactions: React.FC<IProps> = () => {
   const links: string[] = ["overview", "account", "calculator"];
 
-  const selectFilter = (e: any) => {
-    setFilterValue(e.target.value);
-  };
+  const [filterValue, setFilterValue] = useState<string>("Group");
+  const [modalOpened, setModalOpened] = useState<boolean>(false);
+  const [isNewTransaction, setIsNewTransaction] = useState<boolean>(true);
+  const [transactions, setTransactions] = useState<Transaction[]>();
 
-  const formattedDatesFromTransactions: string[] = transactions.map(
-    (transaction: any) => {
-      const formatterdDateArr = transaction.date.split("-");
+  const {
+    chosenDate,
+    userEmail,
+    sortFilter,
+  }: { chosenDate: string; userEmail: string; sortFilter: string } =
+    useSelector((state) => state.baseData);
 
-      return `${formatterdDateArr[2]}.${formatterdDateArr[1]}`;
+  const [transactionDetails, setTransactionDetails] =
+    useState<TransactionDetails>({
+      id: "",
+      name: "",
+      category: "",
+      sum: 0,
+      isLoan: false,
+      date: getDate().fullDate,
+      currency: CURRENCY.CZK,
+      personId: "",
+    });
+
+  const {
+    loading: isFetchingTransactions,
+    error: transactionsError,
+    data: transactionsData,
+    refetch: transactionsRefetch,
+  } = useQuery(GET_TRANSACTIONS, {
+    variables: {
+      email: userEmail,
+    },
+  });
+
+  const {
+    loading: isFetchingUser,
+    error: userError,
+    data: userData,
+    refetch: userRefetch,
+  } = useQuery(GET_USER, {
+    variables: {
+      email: userEmail,
+    },
+  });
+
+  const [updateMutation, { data: updateData, error: updateError }] =
+    useMutation(UPDATE_TRANSACTION);
+
+  const [createMutation, { data: createData, error: createError }] =
+    useMutation(CREATE_TRANSACTION);
+
+  const cleanTransactions: Transaction[] =
+    transactions &&
+    transactions.filter((transaction) => {
+      const transactionYear = transaction.date.split("-")[0];
+      const transactionMonth = transaction.date.split("-")[1];
+
+      const thisMonthTransaction =
+        parseInt(transactionYear) == parseInt(chosenDate.split("-")[0]) &&
+        parseInt(transactionMonth) == parseInt(chosenDate.split("-")[1]);
+
+      return thisMonthTransaction;
+    });
+
+  const sortedData: Transaction[] = useMemo(() => {
+    if (cleanTransactions) {
+      if (sortFilter === "Date") {
+        return cleanTransactions.sort((transaction_1, transaction_2) => {
+          const date_1: number = parseInt(transaction_1.date.split("-")[2]);
+          const date_2: number = parseInt(transaction_2.date.split("-")[2]);
+          return date_1 - date_2;
+        });
+      }
+
+      if (sortFilter === "Amount") {
+        return cleanTransactions.sort((transaction_1, transaction_2) => {
+          return transaction_1.sum - transaction_2.sum;
+        });
+      }
+
+      if (sortFilter === "User") {
+        return cleanTransactions.sort((transaction_1, transaction_2) => {
+          //@ts-ignore
+          return transaction_1.person.name - transaction_2.person.name;
+        });
+      }
     }
-  );
+  }, [sortFilter, cleanTransactions]);
 
-  const spendings: number[] = transactions.map(
-    (transaction: any) => transaction.sum
-  );
+  useEffect(() => {
+    transactionsRefetch();
+    userRefetch();
+
+    let transactions: Transaction[] = [];
+    const teamData = transactionsData?.user.team;
+    teamData &&
+      teamData.forEach((user) => {
+        user.transactions.forEach((transaction) => {
+          transactions.push(transaction);
+        });
+      });
+
+    setTransactions(transactions);
+    setTransactionDetails({
+      ...transactionDetails,
+      personId: userData && userData.user.accountID,
+    });
+  }, [transactionsData, transactionsRefetch, userEmail, userData]);
+
+  const handleTransaction = (): void => {
+    const { id, name, currency, category, sum, isLoan, personId, date } =
+      transactionDetails;
+    const token = localStorage?.getItem("ref_sh_tkn");
+
+    if (isNewTransaction) {
+      createMutation({
+        variables: {
+          transaction: {
+            name,
+            category,
+            sum,
+            currency,
+            date,
+            isLoan,
+            personId,
+          },
+        },
+        context: {
+          headers: {
+            "Content-Type": "application/json",
+            authorization: token,
+          },
+        },
+        refetchQueries: [{ query: GET_TRANSACTIONS }],
+      });
+    } else {
+      updateMutation({
+        variables: {
+          transaction: {
+            id,
+            name,
+            category,
+            sum,
+            date,
+            currency,
+            isLoan,
+          },
+        },
+        context: {
+          headers: {
+            "Content-Type": "application/json",
+            authorization: token,
+          },
+        },
+        refetchQueries: [{ query: GET_TRANSACTIONS }],
+      });
+    }
+
+    transactionsRefetch();
+  };
 
   return (
     <Section sx={{ fontFamily: "Montserrat" }}>
-      <AppBarMenu
-        heading="Transactions"
-        group={true}
-        monthPicker={true}
-        links={links}
-      >
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <ButtonWithIcon
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            onClick={openModal}
-          >
-            <AddCircleIcon fontSize="large" />
-            <ButtonWithIconTypography variant="subtitle1">
-              Add New
-            </ButtonWithIconTypography>
-          </ButtonWithIcon>
-        </Box>
-
-        <FilterSelect
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          defaultValue="Sort by"
-          value={filterValue}
-          onChange={selectFilter}
-        >
-          <MenuItem value="Group"> Date </MenuItem>
-          <MenuItem value="Person 1"> Amount </MenuItem>
-          <MenuItem value="Person 2"> User </MenuItem>
-        </FilterSelect>
-      </AppBarMenu>
-
+      <AppBarMenu heading="Transactions" group={true} monthPicker={true} />
       <GridContainer container sx={{ padding: "20px" }}>
         <TransactionGrid item xs={12} md={5}>
           <MainHeading variant="h4">Transactions</MainHeading>
           <Box sx={{ overflow: "scroll", overflowX: "hidden" }}>
-            {transactions &&
-              transactions.map((transaction: any) => (
+            {sortedData &&
+              sortedData.map((transaction: any, index: number) => (
                 <Transaction
                   name={transaction.name}
                   user={transaction.person}
                   price={transaction.sum}
-                  tags={transaction.tags}
-                  id={transaction._id}
-                  key={transaction._id}
+                  category={transaction.category}
+                  currency={transaction.currency}
+                  isLoan={transaction.isLoan}
+                  date={transaction.date}
+                  id={transaction.id}
+                  key={transaction.id}
+                  transactionDetails={transactionDetails}
+                  setTransactionDetails={setTransactionDetails}
+                  transactionsRefetch={transactionsRefetch}
                 />
               ))}
           </Box>
         </TransactionGrid>
-
         <Grid
           item
           xs={12}
           md={7}
-          sx={{ textAlign: "center", paddingTop: "20px" }}
+          sx={{ textAlign: "center", padding: "160px 70px" }}
         >
-          <MainHeading variant="h4">Spendings By Day</MainHeading>
-          <Box sx={{ height: "400px" }}>
-            <LineGraph
-              labels={formattedDatesFromTransactions}
-              data={spendings}
+          <MainHeading variant="h4">Selected Transaction</MainHeading>
+          <Form component="form">
+            <FormInput
+              label="Name"
+              type="text"
+              value={transactionDetails.name}
+              onChange={(e) => {
+                setTransactionDetails({
+                  ...transactionDetails,
+                  name: e.target.value,
+                });
+              }}
             />
-          </Box>
+            <FormInput
+              label="Category"
+              type="text"
+              value={transactionDetails.category}
+              onChange={(e) => {
+                setTransactionDetails({
+                  ...transactionDetails,
+                  category: e.target.value,
+                });
+              }}
+            />
+            <FormInput
+              label="Sum"
+              type="number"
+              value={transactionDetails.sum}
+              onChange={(e) => {
+                setTransactionDetails({
+                  ...transactionDetails,
+                  sum: parseInt(e.target.value),
+                });
+              }}
+            />
+            <FormInput
+              label="Date"
+              type="date"
+              value={transactionDetails.date}
+              onChange={(e) => {
+                setTransactionDetails({
+                  ...transactionDetails,
+                  date: e.target.value,
+                });
+              }}
+            />
+            <FormSelect
+              labelId="currency-select-label"
+              id="currency-select"
+              defaultValue="Select Currency"
+              value={transactionDetails.currency}
+              onChange={(e: { target: { value: CURRENCY } }) => {
+                setTransactionDetails({
+                  ...transactionDetails,
+                  currency: e.target.value,
+                });
+              }}
+            >
+              <MenuItem value={CURRENCY.CZK}>CZK</MenuItem>
+              <MenuItem value={CURRENCY.EUR}>EUR</MenuItem>
+            </FormSelect>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  onChange={(e) =>
+                    setTransactionDetails({
+                      ...transactionDetails,
+                      isLoan: e.target.checked,
+                    })
+                  }
+                />
+              }
+              label="Is Loan"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  defaultChecked
+                  onChange={(e) => setIsNewTransaction(e.target.checked)}
+                />
+              }
+              label="Is New"
+            />
+            <FormButton variant="contained" onClick={handleTransaction}>
+              submit
+            </FormButton>
+          </Form>
         </Grid>
       </GridContainer>
-
-      <Modal
-        open={modalOpened}
-        onClose={closeModal}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <FormBox component="form">
-          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-            <ButtonWithIconTypography variant="h5">
-              Edit Account
-            </ButtonWithIconTypography>
-            <ButtonWithIcon
-              onClick={closeModal}
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-            >
-              <CancelIcon />
-            </ButtonWithIcon>
-          </Box>
-          <EditTransaction closeModal={closeModal} />
-        </FormBox>
-      </Modal>
     </Section>
   );
-};
-
-export const getServerSideProps = async (): Promise<any> => {
-  const res: Response = await fetch(TRANSACTIONS_URL);
-  const transactions: IProps = await res.json();
-
-  return {
-    props: {
-      transactions,
-    },
-  };
 };
 
 export default Transactions;
