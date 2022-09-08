44922a2a3ee70354b6dab2dc6f9635b39e5b0c36
Karel Geyer <karelgeyer@gmail.com>
2022-09-09 01:50:43 +0200

    feat: create layout folder for wrappers

---
+++ layout/AuthWrapper.tsx
@@ -0,0 +1,41 @@
+import { ReactElement, useContext, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useMutation } from "@apollo/client";
+import { AUTHENTICATE } from "../graphql";
+import { UserSearchContext } from "../state/context/userContext";
+import { useDispatch } from "react-redux";
+import { setEmail } from "../state/reducers";
+
+export interface IProps {
+  children: React.FC | Element | ReactElement;
+}
+
+const AuthWrapper: React.FC<IProps> = ({ children }) => {
+  const router = useRouter();
+  const [authenticate, { data: data, loading: isLoggingIn, error: authError }] =
+    useMutation(AUTHENTICATE);
+  const dispatch = useDispatch();
+
+  useEffect((): any => {
+    const email = localStorage?.getItem("email");
+    dispatch(setEmail(email));
+    const { pathname } = router;
+
+    //   if (pathname != "/login" && pathname != "/") {
+
+    //     if (!email) return router.push("/login");
+
+    //     authenticate({
+    //       variables: { email },
+    //     });
+
+    //     if (authError) return router.push("/login");
+
+    //     if (!data) return router.push("/login");
+    //   }
+  }, [router]);
+
+  return <>{children}</>;
+};
+
+export default AuthWrapper;
