1800c1f88b1a92dd188a57115587c05db363592b
Karel Geyer <karelgeyer@gmail.com>
2022-09-09 02:04:28 +0200

    feat: add authwrapper and remove context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,26 @@
-import React, { useState } from "react";
 import { store } from "../state/store";
 import { Provider } from "react-redux";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
 
-import MaxWidth from "../components/MaxWidth";
+import MaxWidth from "../layout/MaxWidth";
 
 import "../styles/globals.css";
-import { UserSearchContext } from "../state/context/userContext";
-
+import AuthWrapper from "../layout/AuthWrapper";
 const client = new ApolloClient({
   uri: "http://localhost:2000/graphql",
   cache: new InMemoryCache(),
 });
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  const [userSearch, setUserSearch] = useState("");
-
   return (
     <ApolloProvider client={client}>
       <Provider store={store}>
-        <UserSearchContext.Provider value={{ userSearch, setUserSearch }}>
+        <AuthWrapper>
           <MaxWidth>
             <Component {...pageProps} />
           </MaxWidth>
-        </UserSearchContext.Provider>
+        </AuthWrapper>
       </Provider>
     </ApolloProvider>
   );
