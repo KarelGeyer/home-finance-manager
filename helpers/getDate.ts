1afa6eb273956d24c33914c6c2c5bd112d0cadc0
Karel Geyer <karelgeyer@gmail.com>
2022-09-09 02:04:11 +0200

    feat: create helper dir to store helper functions and create a helper function to manage dates

---
+++ helpers/getDate.ts
@@ -0,0 +1,41 @@
+interface FormmatedDate {
+  year: number;
+  month: number | string;
+  monthName: string;
+  date: string;
+  day: string;
+  fullDate: string;
+}
+
+export const getDate = (): FormmatedDate => {
+  const date = new Date();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const day = `${date}`.split(" ")[2];
+
+  const monthNames = [
+    "january",
+    "feburary",
+    "march",
+    "april",
+    "may",
+    "june",
+    "july",
+    "august",
+    "september",
+    "october",
+    "november",
+    "december",
+  ];
+  const currentMonthName = monthNames[month];
+  const currentMonth = month < 10 ? `0${month}` : month;
+
+  return {
+    year,
+    month: currentMonth,
+    day,
+    monthName: currentMonthName,
+    date: `${year}-${currentMonth}`,
+    fullDate: `${year}-${currentMonth}-${day}`,
+  };
+};
