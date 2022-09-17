c2e12401f0e62b0074b7f1d33f4be5d6db7936cc
Karel Geyer <karelgeyer@gmail.com>
2022-09-17 23:50:59 +0200

    feat: add new constants

--- helpers/texts.ts
+++ helpers/texts.ts
@@ -2,6 +2,10 @@ export const ROUTE_NAMES = ["Overview", "Account", "Transactions"];
 
 export const SORT_FILTER_LABELS = ["Date", "Amount", "User"];
 
+export const SORT_USERS = ["Group", "Person1", "Person2"];
+
+export const SORT_TRANSACTION_TYPES = ["All", "Transactions", "Loans"];
+
 export const CATEGORIES = [
   "HOME",
   "FREE_TIME",
