f2b02a48624151db916b0fe8fb55a2d929658d0a
Karel Geyer <karelgeyer@gmail.com>
2022-09-17 23:43:23 +0200

    feat: adjust app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,15 @@
 import { store } from "../state/store";
 import { Provider } from "react-redux";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
-import router from "next/router";
+
+import client from "../apollo-client";
 
 import AuthWrapper from "../layout/AuthWrapper";
 import MaxWidth from "../layout/MaxWidth";
-
-import "../styles/globals.css";
 import Page from "../layout/Page";
 
-const client = new ApolloClient({
-  uri: "http://localhost:2000/graphql",
-  cache: new InMemoryCache(),
-});
+import "../styles/globals.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
